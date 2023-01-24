10c45cc5fd7ea64ce579b975d598ec422cc8baae
Jovan1347 <jovannikolic1347@gmail.com>
2023-01-24 16:34:10 +0100

    new commit

--- root/script.js
+++ root/script.js
@@ -1,22 +1,69 @@
+/* --------- M E N U  B U T T O N --------- */
+
 document.addEventListener("DOMContentLoaded", () => {
   let options = {
     root: null,
-    rootMargin: "-250px 0px",
+    rootMargin: "40% 0%",
     threshold: 1,
   };
 
+  let options2 = {
+    root: null,
+    rootMargin: "-10% 0% 0% 0%",
+    threshold: 1,
+  };
+
+  let observer2 = new IntersectionObserver(beTouching, options2);
+  observer2.observe(document.querySelector(".experience"));
+
   let observer = new IntersectionObserver(beTouching, options);
-  document.querySelectorAll("section").forEach((section) => {
-    observer.observe(section);
-    //console.log("watching", section.textContent);
-  });
+  observer.observe(document.querySelector(".hero"));
 });
+
+let menu = document.querySelectorAll(".menu");
+let menuText = document.querySelectorAll(".main-text");
 
 function beTouching(entries) {
   // entries all sections
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      console.log("intersecting");
+      menu.forEach((item) => {
+        item.classList.remove("changeColor");
+      });
+      menuText.forEach((item) => {
+        item.classList.remove("changeColorText");
+      });
+    } else {
+      menu.forEach((item) => {
+        item.classList.add("changeColor");
+      });
+      menuText.forEach((item) => {
+        item.classList.add("changeColorText");
+      });
     }
   });
 }
+
+/* --------- B A C K  T O  T O P --------- */
+
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 100) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#ffffff ${scrollValue}%, #525252 ${scrollValue}%)`;
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
